9f480f6600eedc1f3f86d8f4632659700fb6fa7e
mariadelosangelesduran <karito_agos@hotmail.com>
2023-04-05 12:42:29 -0300

    mis 2dos archivos en git

---
+++ script.js
@@ -0,0 +1,17 @@
+/* let numero = Number(prompt("Ingrese un numero"))
+ */
+//parseInt, parseFloat, Number
+
+
+
+const nombres = ["mariano", "gaston", "pepe", "maria"]
+
+/* for(let i = 0; i < nombres.length; i++){
+    let nombre = nombres[i]
+    console.log(nombre)
+}
+ */
+//For..of
+for(let nombre of nombres){
+    console.log(nombre)
+}
